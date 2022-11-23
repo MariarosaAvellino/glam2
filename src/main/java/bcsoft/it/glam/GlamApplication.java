52bcf59e8cb8107a830cb1a4a688d1df0de50b75
mariarosa avellino <avellinomariarosa@gmail.com>
2022-11-23 11:56:51 +0100

    Initial commit

---
+++ src/main/java/bcsoft/it/glam/GlamApplication.java
@@ -0,0 +1,13 @@
+package bcsoft.it.glam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlamApplication.class, args);
+	}
+
+}
