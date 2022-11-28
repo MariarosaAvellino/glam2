a2375c00dc1e4ca06b1d7eb0c64c88edf39e0b55
mariarosa avellino <avellinomariarosa@gmail.com>
2022-11-28 11:29:40 +0100

    Update in Utente per criptaggio password (passwordEncoder) Update in AuthService del metodo signUp

--- src/main/java/bcsoft/it/glam/config/SecurityConfig.java
+++ src/main/java/bcsoft/it/glam/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package bcsoft.it.glam.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,6 +19,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    //questo metodo serve a criptare un attributo, nel nostro caso cripteremo la password dell'utente
+    @Bean
+    PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
     }
 
 }
