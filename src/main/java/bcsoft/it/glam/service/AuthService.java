5950e7f7756198c8073fdd83da2c845423dc8e99
GennyMor <gennymorelli00@gmai.com>
2022-11-28 10:51:51 +0100

    Aggiunto package controller con classe SecurityConfig Aggiunto package service con classe AuthService e relativi metodi

---
+++ src/main/java/bcsoft/it/glam/service/AuthService.java
@@ -0,0 +1,11 @@
+package bcsoft.it.glam.service;
+
+import bcsoft.it.glam.model.Utente;
+
+public class AuthService {
+    public void signUp(RegisterRequest registerRequest){
+        Utente user = new Utente();
+        user.setUsername();
+
+    }
+}
