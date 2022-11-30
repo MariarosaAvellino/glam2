facb68d3400706a968c2c72162dfc4a32ceb91b9
Bloisem <Bloisemi@gmail.com>
2022-11-30 13:14:24 +0100

    Mod: annotation @Async

--- src/main/java/bcsoft/it/glam/GlamApplication.java
+++ src/main/java/bcsoft/it/glam/GlamApplication.java
@@ -2,8 +2,10 @@ package bcsoft.it.glam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class GlamApplication {
 	//ciao
 	public static void main(String[] args) {
