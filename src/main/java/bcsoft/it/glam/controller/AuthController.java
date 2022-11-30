b58c0d9c4e1d5a1122043292ea588a12b37f0a58
Bloisem <Bloisemi@gmail.com>
2022-11-30 11:59:21 +0100

    Mod:aggiunto verificaAccount

--- src/main/java/bcsoft/it/glam/controller/AuthController.java
+++ src/main/java/bcsoft/it/glam/controller/AuthController.java
@@ -1,16 +1,11 @@
 package bcsoft.it.glam.controller;
 
 import bcsoft.it.glam.dto.RegisterRequest;
-import bcsoft.it.glam.model.Utente;
 import bcsoft.it.glam.service.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/api/auth")
@@ -23,6 +18,12 @@ public class AuthController {
     public ResponseEntity<String> signup (@RequestBody RegisterRequest registerRequest){
         authService.signup(registerRequest);
         return new ResponseEntity<>("Registrazione avvenuta.", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/verifica/{token}")
+    public ResponseEntity<String> verificaAccount(@PathVariable String token) {
+        authService.verificaAccount(token);
+        return new ResponseEntity<>("Account verificato", HttpStatus.OK);
     }
 
 }
