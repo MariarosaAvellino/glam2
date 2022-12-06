02c635787a0f6464dff00190bd7ab92f9e88e9c0
GennyMor <gennymorelli00@gmai.com>
2022-12-06 10:44:27 +0100

    Creata classe PostMapper con metodi, commentato tutto per sviluppo successivo Creato findAllByPost() in classe CommentoRepository Aggiornato pom con dipendenza marlonlom da github

--- src/main/java/bcsoft/it/glam/repository/CommentoRepository.java
+++ src/main/java/bcsoft/it/glam/repository/CommentoRepository.java
@@ -1,9 +1,13 @@
 package bcsoft.it.glam.repository;
 
 import bcsoft.it.glam.model.Commento;
+import bcsoft.it.glam.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentoRepository extends JpaRepository <Commento, Long> {
+    public List<Commento> findAllByPost(Post post);
 }
